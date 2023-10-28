e956731354f2899ca2669e4208e0fd914b2f43ff
HASURO <rentaro170928@icloud.com>
2023-10-28 21:46:02 +0900

    add: add todo button #3

---
+++ lib/component/button/add_todo_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:namer_app/provider/todos_provider.dart';
+
+class AddTodoButton extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FloatingActionButton(
+      onPressed: () {
+        final notifier = ref.read(todosNotifierProvider.notifier);
+        notifier.addTodo();
+      },
+    );
+  }
+}
