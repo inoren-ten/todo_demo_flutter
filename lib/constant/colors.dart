d3c0d23042392076fe8fff3a391335861a3ff680
HASURO <rentaro170928@icloud.com>
2023-10-28 19:55:24 +0900

    add: color theme

---
+++ lib/constant/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryRed = Color.fromRGBO(217, 10, 39, 1);
+  static const Color primaryBlue = Color.fromRGBO(0, 133, 171, 1);
+  static const Color primaryOrange = Color.fromRGBO(238, 123, 52, 1);
+  static const Color primaryGreen = Color.fromRGBO(45, 166, 0, 1);
+  static const Color primaryGray = Color.fromRGBO(176, 178, 171, 1);
+  static const Color primaryLightYellow = Color.fromRGBO(255, 204, 51, 1);
+  static const Color primaryBlack = Color.fromRGBO(43, 45, 48, 1);
+  static const Color primaryWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const Color primaryPaleGray = Color.fromRGBO(183, 183, 182, 1);
+
+  static const Color textColor = Color.fromRGBO(43, 45, 48, 1);
+}
