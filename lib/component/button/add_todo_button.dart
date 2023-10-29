6755f6fae412fc587d2f3b052c49b69d8ff44f8f
HASURO <rentaro170928@icloud.com>
2023-10-29 17:54:11 +0900

    fix: floatActionButton layout

--- lib/component/button/add_todo_button.dart
+++ lib/component/button/add_todo_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:namer_app/constant/colors.dart';
 import 'package:namer_app/provider/todos_provider.dart';
 
 class AddTodoButton extends ConsumerWidget {
@@ -10,6 +11,12 @@ class AddTodoButton extends ConsumerWidget {
         final notifier = ref.read(todosNotifierProvider.notifier);
         notifier.addTodo();
       },
+      shape: CircleBorder(),
+      backgroundColor: AppColors.primaryBlue,
+      child: Icon(
+        Icons.add,
+        color: AppColors.primaryWhite,
+      ),
     );
   }
 }
