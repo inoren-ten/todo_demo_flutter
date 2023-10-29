8578ef722e0a6af13b243682e60c5fc731cef601
HASURO <rentaro170928@icloud.com>
2023-10-29 17:54:29 +0900

    fix: todo type

--- lib/provider/todos_provider.dart
+++ lib/provider/todos_provider.dart
@@ -6,6 +6,7 @@ part 'todos_provider.g.dart';
 
 @riverpod
 class TodosNotifier extends _$TodosNotifier {
+// ignore: avoid_public_notifier_properties
   Map<String, dynamic> initialTodo = {'title': 'title', 'finish': false};
 
   @override
@@ -15,10 +16,21 @@ class TodosNotifier extends _$TodosNotifier {
 
   void addTodo() {
     final TodoType newTodo = TodoType(
+      id: state.length,
       title: 'task',
       finish: false,
     );
     final newState = [...state, newTodo];
     state = newState as List<TodoType>;
+  }
+
+  void completeTodo({required int id}) {
+    int index = state.indexWhere((element) => element.id == id);
+    if (index != -1) {
+      state[index] = TodoType(
+          id: state[index].id,
+          title: state[index].title,
+          finish: !state[index].finish);
+    }
   }
 }
