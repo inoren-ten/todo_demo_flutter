5bef0fc38407d55be83848af3a2e1a3f84852556
HASURO <rentaro170928@icloud.com>
2023-11-04 12:21:51 +0900

    fix: add todo page style #13

--- lib/view/add_todo_page.dart
+++ lib/view/add_todo_page.dart
@@ -11,53 +11,90 @@ class AddTodoPage extends ConsumerStatefulWidget {
 class _AddTodoPageState extends ConsumerState<AddTodoPage> {
   var title = '';
   var backgroundColor = AppColors.primaryPaleGray;
+  var bsckground;
 
   @override
   Widget build(BuildContext context) {
+    if (title.length > 0) {
+      backgroundColor = AppColors.primaryBlue;
+    } else {
+      backgroundColor = AppColors.primaryPaleGray;
+    }
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primaryWhite,
-        foregroundColor: AppColors.textColor,
-        leading: TextButton(
-          child: Icon(
-            Icons.clear,
-            color: AppColors.textColor,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: Text('Todoを追加'),
-        actions: [
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: title.length > 0
-                  ? AppColors.primaryBlue
-                  : AppColors.primaryPaleGray,
+        appBar: AppBar(
+          backgroundColor: AppColors.primaryWhite,
+          foregroundColor: AppColors.textColor,
+          leading: TextButton(
+            child: Icon(
+              Icons.clear,
+              color: AppColors.textColor,
             ),
-            child: Text('追加'),
             onPressed: () {
-              if (title.length > 0) {
-                final notifier = ref.read(todosNotifierProvider.notifier);
-                notifier.addTodo(title: title);
-                Navigator.of(context).pop();
-              }
+              Navigator.of(context).pop();
             },
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Todoのタイトル',
-            ),
-            onChanged: (String value) {
-              title = value;
-            },
-          )
-        ],
-      ),
-    );
+          ),
+          title: Text('Todoを追加'),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: backgroundColor,
+              ),
+              child: Text(
+                '追加',
+                style: TextStyle(
+                  color: AppColors.primaryWhite,
+                ),
+              ),
+              onPressed: () {
+                if (title.length > 0) {
+                  final notifier = ref.read(todosNotifierProvider.notifier);
+                  notifier.addTodo(title: title);
+                  Navigator.of(context).pop();
+                }
+              },
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('タイトル',
+                      style: TextStyle(
+                          color: AppColors.textColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600)),
+                  Text('${title.length}/20',
+                      style: TextStyle(
+                          color: AppColors.textColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600)),
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Todoのタイトル',
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.primaryBlue))),
+                cursorColor: AppColors.primaryBlue,
+                onChanged: (String value) {
+                  setState(() {
+                    title = value;
+                  });
+                },
+                onSubmitted: (String value) {
+                  if (value.length > 0) {
+                    final notifier = ref.read(todosNotifierProvider.notifier);
+                    notifier.addTodo(title: value);
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
