3265a4c039b5806c45ccfcd22c8170fd1a8f6782
HASURO <rentaro170928@icloud.com>
2023-11-09 16:28:22 +0900

    add: change color theme #18

---
+++ lib/provider/color_theme_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'color_theme_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$colorThemeNotifierHash() =>
+    r'613457512cc64d18e1117bc8bc00937382cbde24';
+
+/// See also [ColorThemeNotifier].
+@ProviderFor(ColorThemeNotifier)
+final colorThemeNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<ColorThemeNotifier, String>.internal(
+  ColorThemeNotifier.new,
+  name: r'colorThemeNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$colorThemeNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ColorThemeNotifier = AutoDisposeAsyncNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
